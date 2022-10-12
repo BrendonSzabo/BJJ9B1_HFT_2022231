8a945197fabffa10e7279acdd60abd0011c69146
Szabó Brendon <lord.nelson03@gmail.com>
2022-10-12 11:08:39 +0200

    idriver

---
+++ BJJ9B1_HFT_2022231.Logic/IDriver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJJ9B1_HFT_2022231.Logic
+{
+    public interface IDriver
+    {
+        IEnumerable<Drivers> ReadAll();
+        void Create(Drivers item);
+        void Delete(int id);
+        void Update(Drivers item);
+        Drivers Read(int id);
+
+    }
+}
