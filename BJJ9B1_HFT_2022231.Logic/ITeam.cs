e2c9db633ebd878485eb659fc8910283ffea7660
Szabó Brendon <lord.nelson03@gmail.com>
2022-10-12 11:08:15 +0200

    iteam

---
+++ BJJ9B1_HFT_2022231.Logic/ITeam.cs
@@ -0,0 +1,19 @@
+﻿using BJJ9B1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJJ9B1_HFT_2022231.Logic
+{
+    public interface ITeam
+    {
+        IEnumerable<Drivers> ReadAll();
+        void Update(Drivers item);
+        void Create(Drivers item);
+        void Delete(int id);
+        Drivers Read(int id);
+        
+    }
+}
