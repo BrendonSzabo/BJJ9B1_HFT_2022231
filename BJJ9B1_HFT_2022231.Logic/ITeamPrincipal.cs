3eccc1429d3ec88aa7eb278603a9459db3b60ec4
Szabó Brendon <lord.nelson03@gmail.com>
2022-10-12 11:13:58 +0200

    itp fill

--- BJJ9B1_HFT_2022231.Logic/ITeamPrincipal.cs
+++ BJJ9B1_HFT_2022231.Logic/ITeamPrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BJJ9B1_HFT_2022231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace BJJ9B1_HFT_2022231.Logic
 {
     internal interface ITeamPrincipal
     {
+        IEnumerable<Drivers> ReadAll();
+        void Create(Drivers item);
+        void Update(Drivers item);
+        void Delete(int id);
+        Drivers Read(int id);
     }
 }
