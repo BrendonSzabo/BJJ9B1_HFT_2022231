4f70c8ad6937d1e74fad34024dbe0d1901b41a5b
Szabó Brendon <lord.nelson03@gmail.com>
2022-10-12 10:55:56 +0200

    Principal Repo

---
+++ BJJ9B1_HFT_2022231.Repository/TeamPrincipalRepository.cs
@@ -0,0 +1,30 @@
+﻿using BJJ9B1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJJ9B1_HFT_2022231.Repository
+{
+    public class TeamPrincipalRepository : Repository<TeamPrincipals>, IRepository<TeamPrincipals>
+    {
+        public TeamPrincipalRepository(F1DbContext DbCont) : base(DbCont)
+        {
+        }
+
+        public override TeamPrincipals Read(int id)
+        {
+            return DbCont.Principals.FirstOrDefault(x => x.id == id);
+        }
+
+        public override void Update(TeamPrincipals item)
+        {
+            var o = Read(item.id);
+            foreach (var tmp in o.GetType().GetProperties())
+            {
+                tmp.SetValue(o, tmp.GetValue(item));
+            }
+        }
+    }
+}
---
+++ BJJ9B1_HFT_2022231.Repository/TeamRepository.cs
@@ -0,0 +1,26 @@
+﻿using BJJ9B1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJJ9B1_HFT_2022231.Repository
+{
+    public class TeamRepository : Repository<Teams>, IRepository<Teams>
+    {
+        public TeamRepository(F1DbContext DbCont) : base(DbCont)
+        {
+        }
+
+        public override Teams Read(int id)
+        {
+            return DbCont.Teams.FirstOrDefault(x => x.id == id);
+        }
+
+        public override void Update(Teams item)
+        {
+            var o = Read(item.id);
+        }
+    }
+}
