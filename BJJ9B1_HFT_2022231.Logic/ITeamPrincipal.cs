16ec34fc86cb736c00902d2602bdcdef9f9849f3
Szabó Brendon <lord.nelson03@gmail.com>
2022-10-12 11:09:16 +0200

    itp

---
+++ BJJ9B1_HFT_2022231.Logic/ITeamPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJJ9B1_HFT_2022231.Logic
+{
+    internal interface ITeamPrincipal
+    {
+    }
+}
