2165235b84e1ac26487dbbc62e788e75a9289331
Szabó Brendon <brendon.szabo@etriad.hu>
2022-10-17 17:03:13 +0200

    TeamPrinciaplLogic created

---
+++ BJJ9B1_HFT_2022231.Logic/TeamPrincipalLogic.cs
@@ -0,0 +1,41 @@
+﻿using BJJ9B1_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJJ9B1_HFT_2022231.Logic
+{
+    internal class TeamPrincipalLogic : ITeamPrincipal
+    {
+        public TeamPrincipalLogic()
+        {
+        }
+
+        void ITeamPrincipal.Create(Drivers item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ITeamPrincipal.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Drivers ITeamPrincipal.Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Drivers> ITeamPrincipal.ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ITeamPrincipal.Update(Drivers item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
