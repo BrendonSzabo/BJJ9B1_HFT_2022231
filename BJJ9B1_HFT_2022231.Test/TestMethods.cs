823c91d4cb57575c34da17f2ee2e011826dccb7d
Szabó Brendon <brendon.szabo@etriad.hu>
2022-10-17 17:58:50 +0200

    Teast packages added

---
+++ BJJ9B1_HFT_2022231.Test/TestMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using BJJ9B1_HFT_2022231.Logic;
+using BJJ9B1_HFT_2022231.Models;
+using BJJ9B1_HFT_2022231.Repository;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Collections.Generic;
+
+namespace BJJ9B1_HFT_2022231.Test
+{
+    [TestFixture]
+    public class TestMethods
+    {
+    }
+}
