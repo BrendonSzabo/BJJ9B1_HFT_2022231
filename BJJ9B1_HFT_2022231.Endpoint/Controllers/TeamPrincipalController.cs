9b2b8e592d21efae783aac02db73e0ad1f1b3eaa
Szabó Brendon <brendon.szabo@etriad.hu>
2022-10-20 13:03:37 +0200

    team principal controller finished

--- BJJ9B1_HFT_2022231.Endpoint/Controllers/TeamPrincipalController.cs
+++ BJJ9B1_HFT_2022231.Endpoint/Controllers/TeamPrincipalController.cs
@@ -1,6 +1,49 @@
-﻿namespace BJJ9B1_HFT_2022231.Endpoint.Controllers
+﻿using BJJ9B1_HFT_2022231.Logic;
+using BJJ9B1_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BJJ9B1_HFT_2022231.Endpoint.Controllers
 {
-    public class TeamPrincipalController
+    [Route("[controller]")]
+    [ApiController]
+    public class TeamPrincipalController : ControllerBase
     {
+        ITeamPrincipal logic;
+
+        public TeamPrincipalController(ITeamPrincipal logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<TeamPrincipals> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public TeamPrincipals Get(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] TeamPrincipals value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] TeamPrincipals value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
