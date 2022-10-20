6e4d28c052691e2cf50c3e3cbd0dade5f7c9ccb3
Szabó Brendon <brendon.szabo@etriad.hu>
2022-10-20 12:53:38 +0200

    Driverr controller made

--- BJJ9B1_HFT_2022231.Endpoint/Controllers/DriverController.cs
+++ BJJ9B1_HFT_2022231.Endpoint/Controllers/DriverController.cs
@@ -1,6 +1,44 @@
-﻿namespace BJJ9B1_HFT_2022231.Endpoint.Controllers
+﻿using BJJ9B1_HFT_2022231.Logic;
+using BJJ9B1_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BJJ9B1_HFT_2022231.Endpoint.Controllers
 {
-    public class DriverController
+    [Route("[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
     {
+        IDriver logic;
+
+        public DriverController(IDriver logic)
+        {
+            this.logic = logic;
+        }
+        [HttpGet]
+        public IEnumerable<Drivers> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+        [HttpPost]
+        public void Create([FromBody] Drivers value)
+        {
+            this.logic.Create(value);
+        }
+        [HttpGet("{id}")]
+        public Drivers Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+        [HttpPut]
+        public void Update([FromBody] Drivers value)
+        {
+            this.logic.Update(value);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
