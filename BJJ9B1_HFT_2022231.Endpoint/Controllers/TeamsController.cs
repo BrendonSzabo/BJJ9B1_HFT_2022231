7bad490898ff2a942c19180974a453c41fccbb45
Szabó Brendon <brendon.szabo@etriad.hu>
2022-10-20 13:05:19 +0200

    team controller finished

--- BJJ9B1_HFT_2022231.Endpoint/Controllers/TeamsController.cs
+++ BJJ9B1_HFT_2022231.Endpoint/Controllers/TeamsController.cs
@@ -1,6 +1,48 @@
-﻿namespace BJJ9B1_HFT_2022231.Endpoint.Controllers
+﻿using BJJ9B1_HFT_2022231.Logic;
+using BJJ9B1_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BJJ9B1_HFT_2022231.Endpoint.Controllers
 {
-    public class TeamsController
+    [Route("[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
     {
+        ITeam logic;
+
+        public TeamsController(ITeam logic)
+        {
+            this.logic = logic;
+        }
+        [HttpGet]
+        public IEnumerable<Teams> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Teams Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Teams value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Teams value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
